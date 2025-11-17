f52f91ad707a2a46b11a8644ff2a8cacc066c4c0
Nkeiru Lois <nkeiruloisy@yahoo.com>
2025-11-17 23:54:12 +0100

    index.tsx updates

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,5 +1,47 @@
-export interface UserData {
+export interface PostProps {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export interface PostData {
+  userId: number;
   id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
+
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export interface UserData {
   name: string;
   username: string;
   email: string;
@@ -24,5 +66,5 @@ export interface UserData {
 
 export interface UserModalProps {
   onClose: () => void;
-  onSubmit: (post: UserProps) => void;
+  onSubmit: (user: UserData) => void;
 }
